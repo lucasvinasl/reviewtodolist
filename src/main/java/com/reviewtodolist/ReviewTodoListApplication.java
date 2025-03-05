ca81ebbb18f067ef33af7d83c708d8cc69cb197f
Lucas Vinícius <lucasvinicius.ctec@gmail.com>
2025-03-05 15:30:17 -0300

    subindo o git test do todolist

---
+++ src/main/java/com/reviewtodolist/ReviewTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.reviewtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reviewtodolist/ReviewTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
